0572189cc0eab598fd99fce5c68524806e678712
Marie290 <98214204+Marie290@users.noreply.github.com>
2025-02-09 14:53:49 +0800

    Add min_length into the function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,7 +2,7 @@ MIN_LENGTH = 4
 
 def main():
     """Get password and print asterisks"""
-    user_password = get_password()
+    user_password = get_password(MIN_LENGTH)
 
     print_asterisks(user_password)
 
@@ -12,12 +12,12 @@ def print_asterisks(user_password):
     print("*" * len(user_password))
 
 
-def get_password():
+def get_password(min_length):
     """Get password and check its validity"""
-    user_password = input(f"Enter a password of minimum length {MIN_LENGTH}: ")
-    while len(user_password) < MIN_LENGTH:
-        print(f"Password must be at least {MIN_LENGTH} long")
-        user_password = input(f"Enter a password of minimum length {MIN_LENGTH}: ")
+    user_password = input(f"Enter a password of minimum length {min_length}: ")
+    while len(user_password) < min_length:
+        print(f"Password must be at least {min_length} long")
+        user_password = input(f"Enter a password of minimum length {min_length}: ")
     return user_password
 
 
