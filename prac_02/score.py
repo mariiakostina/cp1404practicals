712b193a4656f62dbd4c17ca49e21d973f2182cf
Marie290 <98214204+Marie290@users.noreply.github.com>
2025-02-09 15:51:59 +0800

    Wrote the code

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,17 +3,30 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+import random
+
 MIN_SCORE = 0
 MAX_SCORE = 100
 PASSABLE_THRESHOLD = 50
 EXCELLENT_THRESHOLD = 90
 
-score = float(input("Enter score: "))
-if score < MIN_SCORE or score > MAX_SCORE:
-    print("Invalid score")
-elif score >= EXCELLENT_THRESHOLD:
-    print("Excellent")
-elif score >= PASSABLE_THRESHOLD:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """Get score, print score, random score"""
+    score = float(input("Enter score: "))
+    print(f"Your result: {get_score_result(score)}")
+
+    random_score = random.randint(MIN_SCORE, MAX_SCORE)
+    print(f"Random score {random_score}: {get_score_result(random_score)}")
+
+def get_score_result(score):
+    """Returns result based on the score"""
+    if score < MIN_SCORE or score > MAX_SCORE:
+        return "Invalid score"
+    elif score >= EXCELLENT_THRESHOLD:
+        return "Excellent"
+    elif score >= PASSABLE_THRESHOLD:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
