81897f0f29c4645af62c032824aadb14b888a7f0
Marie290 <98214204+Marie290@users.noreply.github.com>
2025-02-23 16:32:54 +0800

    Adjusted the code

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,32 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
-    print(data)
+    subjects = load_data()
+    display_subject_details(subjects)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    course_details = []
+
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        # print("----------")
+        course_details.append(parts)
     input_file.close()
+    return course_details
+
+def display_subject_details(subjects):
+    """Display the subject details"""
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:3} students")
 
 
 main()
