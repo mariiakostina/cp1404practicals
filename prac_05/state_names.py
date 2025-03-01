d11643340c3e8309d2ab697f46fce222e236d0f2
Marie290 <98214204+Marie290@users.noreply.github.com>
2025-03-02 00:36:34 +0800

    Reformated the file to follow PEP 8, added .upper(), wrote a loop,changed for EAFP approach with try-except

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -7,12 +7,21 @@ File needs reformatting
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
+
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+state_code = input("Enter short state: ").upper()
+
+while state_code:
+    try:
+        print(state_code, "is", CODE_TO_NAME[state_code], "\n")
+
+        print("\nFull list of states:")
+        for code, name in CODE_TO_NAME.items():
+            print(f"{code:4} is {name}")
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+
+    state_code = input("Enter short state: ").upper()
+
+
