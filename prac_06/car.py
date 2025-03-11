5958848e3425cf0b2b2c035a4f1e88e7ba2ec45a
Marie290 <98214204+Marie290@users.noreply.github.com>
2025-03-11 23:03:32 +0800

    Completed cars.py, according to the tasks.

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,11 +4,13 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name, fuel=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
+
+        self.name = name
         self.fuel = fuel
         self._odometer = 0
 
@@ -29,3 +31,7 @@ class Car:
             self.fuel -= distance
         self._odometer += distance
         return distance
+
+    def __str__(self):
+        """Return a descriptive string, instead of print"""
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
