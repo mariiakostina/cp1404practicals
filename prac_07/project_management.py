ff30fc34877d1dd709d027e05766b03dbdad5f7a
Marie290 <98214204+Marie290@users.noreply.github.com>
2025-03-23 19:45:01 +0800

    Add get_positive_number error-checking, completed main() function, add new function (add_project)

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -34,7 +34,15 @@ def main():
            add_project(projects)
        elif option == "U":
            update_project(projects)
+        else:
+            print("Invalid menu choice")
 
+save_option = input(f"Would you like to save to {FILENAME}? (y/n): ").strip().lower()
+if save_option == "y":
+    save_projects(FILENAME, projects)
+    print("Thank you for using custom-built project management software.")
+else:
+    print("Invalid menu choice, please enter a valid option.")
 def load_projects(filename):
     """Load projects from a text file."""
     projects = []
@@ -65,7 +73,7 @@ def display_projects(projects):
 
 def filter_projects_date(projects):
     """Filter projects that start after a given date."""
-    filter_date = get_valid_date("Show projects that start after date (dd/mm/yyyy): ")
+    filter_date = get_valid_date ("Show projects that start after date (dd/mm/yyyy): ")
     print(f"That day is/was {filter_date.strftime('%A')}")
     print(filter_date.strftime("%d/%m/%Y"))
     print("Filtered projects:")
@@ -82,10 +90,34 @@ def get_valid_date(prompt):
     except ValueError:
         print("Invalid date format. Use dd/mm/yyyy.")
 
+def get_valid_input(promt):
+    value = input(promt).strip()
+    if value != "":
+        return value
+    else:
+        return "Value cannot be blank"
+
+def get_positive_number(prompt):
+    """Ensure input is positive"""
+    try:
+        value = number_type(input(prompt).strip())
+        if value > 0:
+            return value
+    except ValueError:
+        print(f"Invalid input. Please enter a valid {number_type.__name__}.")
 
 def add_project(projects):
     """Add new project"""
     print("Let's add a new project!")
+
+    name = et_valid_input ("Name: ")
+    start_date = get_valid_date("Start date (dd/mm/yy): ")
+    priority = get_positive_number("Priority: ", int)
+    cost = get_positive_number("Cost estimate: $", float)
+    completion = get_positive_number("Percent complete: ", int)
+
+    projects.append([name, start_date.strftime("%d/%m/%Y"), priority, cost, completion])
+
 
 def update_project(projects):
 
