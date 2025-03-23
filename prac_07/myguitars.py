00b97f8054cc1282780a1b68a1cb5335288248c6
Marie290 <98214204+Marie290@users.noreply.github.com>
2025-03-23 17:52:19 +0800

    Changed the intergace of the code, so it asked y/n it want to sort or add a new guitar

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -19,10 +19,21 @@ def main():
 
     in_file.close()
 
-    print("Sort guitars by year: ")
-    guitars.sort()
+    print("Loaded Guitars ")
     display_guitars(guitars)
-    add_new_guitars(guitars)
+
+    choice_to_sort = input("\nDo you want to sort guitars by year? (y/n): ").strip().lower()
+    if choice_to_sort == "y":
+        guitars.sort()
+        print("Sorted Guitars:")
+        display_guitars(guitars)
+
+
+    choice_add_guitar = input("\nDo you want to add new guitars? (y/n): ").strip().lower()
+    if choice_add_guitar == "y":
+        add_new_guitars(guitars)
+
+
     store_guitars(guitars)
 
 def display_guitars(guitars):
@@ -30,7 +41,6 @@ def display_guitars(guitars):
     if not guitars:
         print("Guitars not found")
     else:
-        print("Guitars:")
         for guitar in guitars:
             print(guitar)
 
@@ -48,11 +58,11 @@ def add_new_guitars(guitars):
 
 def store_guitars(guitars):
     """Save current list of guitars to a text file"""
-    in_file = open("guitars.csv", "w", newline="")
+    out_file = open("guitars.csv", "w", newline="")
     for guitar in guitars:
-        in_file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
+        out_file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
 
-    in_file.close()
+    out_file.close()
     print("\nGuitars saved to 'guitars.csv'!")
 
 if __name__ == "__main__":
